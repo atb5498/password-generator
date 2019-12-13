f3256ae6853177b0f75cd2fe2cdd4bab5b8fa69d
abickford <atb5498@gmail.com>
2019-12-12 20:21:48 -0600

    created conditionals for all character types in script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function generatePassword(passwordLength) {
+function generatePassword(passwordLength, useLowercase, useUppercase, useNumbers, useSpecialCharacters) {
     if (passwordLength < 8 || passwordLength > 128) {
         throw new Error('Password length must be greater than or equal to 8 and less than or equal to 128');
     }
 
-    var charSet = "abcdefghij";
+    var charSet = "";
+
+    if (useLowercase === true) {
+        charSet += "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    if (useUppercase === true) {
+        charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    if (useNumbers === true) {
+        charSet += "0123456789";
+    }
+
+    if (useSpecialCharacters === true) {
+        charSet += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    }
+
     var generatedPassword = "";
 
     for (var i = 0; i < passwordLength; i++) {
@@ -14,4 +31,4 @@ function generatePassword(passwordLength) {
     return generatedPassword;
 }
 
-console.log(generatePassword(8));
+console.log(generatePassword(20, true, true, false, true));
