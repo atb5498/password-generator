d67b5efbe12e5ad8607b57f950b4bdd64aae682a
abickford <atb5498@gmail.com>
2019-12-25 19:17:20 -0600

    created .gitignore and reformatted readme.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Password Generator
 
-This project is intended to produce a random password generator based on user-selected criteria. The application validates user input and ensures that at least one character type is selected. Once all prompts are answered, the user will be presented with a password matching the answered prompts. The user will have the option to copy the generated password to the clipboard by clicking the COPY button.
+This project is intended to produce a random password generator based on user-selected criteria. The application validates user input and ensures the generated password contains between 8 and 128 characters, and is made up of at least one character type. Once all prompts are answered, the user will be presented with a randomly generatd password as a result of the user's response. The user will have the option to copy the generated password to the clipboard by clicking the COPY button.
 
 # Getting Started
 
@@ -9,7 +9,8 @@ This project is intended to produce a random password generator based on user-se
 3. The user is asked to confirm whether to include lowercase letters?
 4. The user is asked to confirm whether to include uppercase letters?
 5. The user is asked to confirm whether to include numbers?
-6. The user is asked to confirm whether to special characters?
+6. The user is asked to confirm whether to include special characters?
+7. Randomly generated password is displyed 
 
 # Built With
 
@@ -17,6 +18,6 @@ Bootstrap - https://getbootstrap.com/
 
 # Link
 
-https://atb5498.github.io/homework5/
+https://atb5498.github.io/homework3/
 
 ![alt text](screen.png "Logo Title Text 1")
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function generatePassword() {
+    //Asks user to determine password length and character set
     var passwordLength = parseInt(prompt("Enter a character length between 8 and 128"));
     var useLowercase = confirm("Would you like to include lowercase letters?");
     var useUppercase = confirm("Would you like to include uppercase letters?");
@@ -6,6 +7,7 @@ function generatePassword() {
     var useSpecialCharacters = confirm("Would you like to include special characters?");
     var charSet = "";
 
+    //Determines generated password is between 8 and 128 characters long
     if (passwordLength < 8) {
         alert("Password length must be greater than or equal to 8 characters");
         return;
@@ -16,6 +18,7 @@ function generatePassword() {
         return;
     }
 
+    //Enforces at least one character set is chosen
     if (useLowercase === false && useUppercase === false && useNumbers === false && useSpecialCharacters === false) {
         alert("Password must include atleast one character type");
         return;
@@ -39,14 +42,17 @@ function generatePassword() {
 
     var generatedPassword = "";
 
+    //Randomizes characters based on chosen character limit and character sets
     for (var i = 0; i < passwordLength; i++) {
         var characterIndex = Math.floor(Math.random() * charSet.length);
         generatedPassword += charSet.charAt(characterIndex);
     }
 
+    //Displays generated password
     document.getElementById("generated-password").textContent = generatedPassword;
 }
 
+//Copies generated password to clipboard
 function copyPassword() {
     var copyText = document.getElementById("generated-password");
     copyText.select();
